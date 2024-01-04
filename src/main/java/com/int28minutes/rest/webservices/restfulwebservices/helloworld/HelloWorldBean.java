119aefd7878af697e8447dba05a4d77eeeacb1e4
Dlsdn <wkddlsdn@gmail.com>
2024-01-05 00:24:42 +0900

    really last

--- src/main/java/com/int28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/int28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,2 +1,31 @@
-package com.int28minutes.rest.webservices.restfulwebservices.helloworld;public class HelloWorldBean {
+package com.int28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldBean {
+
+    private String name;
+
+    public HelloWorldBean() {
+    }
+
+    public HelloWorldBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/int28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/int28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,2 +1,17 @@
-package com.int28minutes.rest.webservices.restfulwebservices.helloworld;public class HelloWorldController {
+package com.int28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(
+                String.format("Hello World","%s", name));
+    }
+
+
 }
